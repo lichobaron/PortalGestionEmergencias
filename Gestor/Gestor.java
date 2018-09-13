98562b0b5f0fb48cde7e443f32cb25f500242121
LICHO <lichobaron@gmail.com>
2018-09-13 14:04:08 -0500

    Hilos principales

--- Gestor/Gestor.java
+++ Gestor/Gestor.java
@@ -2,27 +2,162 @@ package javeriana.edu.co;
 
 import java.net.*;
 import java.io.*;
+import java.util.concurrent.*;
 
 import javeriana.edu.co.Mensaje;
 
 class Gestor {
+
+	public static enum Tipo {
+		SUBSCLIE, SUBSFUEN, NOTICI
+	}
+
+	public static ConcurrentLinkedQueue<Mensaje> colaSubscripcionesCliente;
+	public static ConcurrentLinkedQueue<Mensaje> colaSubscripcionesFuente;
+	public static ConcurrentLinkedQueue<Mensaje> colaEnviodeMensajes;
+
 	public static void main(String[] args) {
+		colaSubscripcionesCliente = new ConcurrentLinkedQueue<>(); 
+		colaSubscripcionesFuente = new ConcurrentLinkedQueue<>(); 
+		colaEnviodeMensajes = new ConcurrentLinkedQueue<>(); 
+
+		ClienteThread clienteThread = new ClienteThread(colaSubscripcionesCliente);
+		FuenteThread fuenteThread = new FuenteThread(colaSubscripcionesFuente);
+		PublishMessageThread publishMessageThread = new PublishMessageThread();
+
+		clienteThread.start();
+		fuenteThread.start();
+		publishMessageThread.start();
+
 		try {
 			DatagramSocket serverSocket = new DatagramSocket(9876);
 			byte[] receiveData = new byte[1024];
 			while (true) {
+
 				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 				serverSocket.receive(receivePacket);
 				byte[] data = receivePacket.getData();
 				ObjectInputStream iStream = new ObjectInputStream(new ByteArrayInputStream(data));
-				Mensaje prueba = (Mensaje)iStream.readObject();
+				Mensaje mensaje = (Mensaje)iStream.readObject();
 				iStream.close();
-				if (prueba != null) 
-					System.out.println(prueba);
+				
+				if (mensaje != null){
+					System.out.println(mensaje);
+					switch(mensaje.getTipo()) {
+						case SUBSCLIE:
+							System.out.println("Esperando subscripción cliente");
+							colaSubscripcionesCliente.add(mensaje);
+						   	break;
+						case SUBSFUEN:
+							System.out.println("Esperando subscripción fuente");
+							colaSubscripcionesFuente.add(mensaje);
+						   	break;
+						case NOTICI:
+							System.out.println("Esperando envío de noticias");
+							colaEnviodeMensajes.add(mensaje);
+						   	break;
+						default :
+							System.out.println("Mensaje inválido"); 
+						   	break;
+					 }				
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(e);
 		}
+	}
+}
+
+class ClienteThread extends Thread {
+
+	private ConcurrentLinkedQueue<Mensaje> colaSubscripcionesCliente;
+
+	public ClienteThread(ConcurrentLinkedQueue<Mensaje> colaSubscripcionesCliente){
+		this.colaSubscripcionesCliente = colaSubscripcionesCliente;
+	}
+
+	public void run () {
+		/*
+		while(true){
+			if(colaSubscripcionesCliente.peek()!=null){
+				System.out.println("Subscripción cliente terminada!"); 
+				colaSubscripcionesCliente.remove();
+			}
+		}*/  
+	  	try {
+			while(true){
+				if(colaSubscripcionesCliente.peek()!=null){
+					sleep(10000);
+					System.out.println("Subscripción cliente terminada!"); 
+					colaSubscripcionesCliente.remove();
+				}
+			} 
+	  	} catch (InterruptedException e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
+	}
+}
+
+class FuenteThread extends Thread {
+
+	private ConcurrentLinkedQueue<Mensaje> colaSubscripcionesFuente;
+
+	public FuenteThread(ConcurrentLinkedQueue<Mensaje> colaSubscripcionesFuente){
+		this.colaSubscripcionesFuente = colaSubscripcionesFuente;
+	}
+
+	public void run () {
+		while(true){
+			if(colaSubscripcionesFuente.peek()!=null){
+				System.out.println("Subscripción fuente terminada!");
+				colaSubscripcionesFuente.remove();
+			}
+		} 
+		/*
+		try {
+			while(true){
+				if(colaSubscripcionesFuente.peek()!=null){
+					System.out.println("Subscripción fuente terminada!");
+					colaSubscripcionesFuente.remove();
+				}
+			} 
+	  	} catch (InterruptedException e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		*/
+	}
+}
+
+class PublishMessageThread extends Thread {
+
+	private ConcurrentLinkedQueue<Mensaje> colaEnviodeMensajes;
+
+	public PublishMessageThread(ConcurrentLinkedQueue<Mensaje> colaEnviodeMensajes){
+		this.colaEnviodeMensajes = colaEnviodeMensajes;
+	}
+
+	public void run () {
+		while(true){
+			if(colaEnviodeMensajes.peek()!=null){
+				System.out.println("Envío de mensajes terminado!"); 
+				colaEnviodeMensajes.remove();
+			}
+		} 
+		/*
+		try {
+			while(true){
+				if(colaEnviodeMensajes.peek()!=null){
+					System.out.println("Envío de mensajes terminado!"); 
+					colaEnviodeMensajes.remove();
+				}
+			} 
+	  	} catch (InterruptedException e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		*/
 	}
 }
